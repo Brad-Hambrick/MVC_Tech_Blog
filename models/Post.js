3ace9ea3f544fe1cac5ca6be1486d043875f5e86
Bradley Hambrick <bradleyhambrick@Bradleys-MacBook-Pro.local>
2022-11-05 20:17:06 -0500

    created models and began working on routes

---
+++ models/Post.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // Used data type TEXT in order to have less limit on the characters
+        post_content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                Model: 'user',
+                key: 'id'
+            }
+        }
+    },
+
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
+
+module.exports = Post;
